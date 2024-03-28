b80e55a7852e7a6d929c1c12d501048bf93368ea
Anjalee Kulasinghe <anjaleek@gmail.com>
2024-03-28 23:09:32 +0000

    Create article model

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db import models
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Article(models.Model):
+	title = models.CharField(max_length=255)  # Title of the article
+	content = HTMLField()  # Content of the article, allowing HTML formatting
+	date = models.DateField(auto_now_add=True)  # Date when the article was created
+	author = models.ForeignKey(User, on_delete=models.CASCADE)  # Author of the article
+	featured = models.BooleanField(default=False)  # Indicates if the article is featured
+	likes = models.ManyToManyField(User, related_name='likes', blank=True)  # Users who liked the article
--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import Index
 
 urlpatterns = [
+    path('tinymce/', include('tinymce.urls')),
     path('', Index.as_view(), name='index'),
 ]
