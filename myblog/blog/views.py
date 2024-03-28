a10bf4eacfa5884e0a49859d247c90f5ac704bfe
Anjalee Kulasinghe <anjaleek@gmail.com>
2024-03-28 23:35:19 +0000

    Modify the index page and add pagination

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, DetailView, DeleteView
+from .models import Article
 
-class Index(View):
-    def get(self, request):
-        return render(request, 'blog/index.html')
+class Index(ListView):
+	model = Article
+	queryset = Article.objects.all().order_by('-date')
+	template_name = 'blog/index.html'
+	paginate_by = 1
