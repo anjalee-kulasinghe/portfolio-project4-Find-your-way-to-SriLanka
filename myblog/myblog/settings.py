773edce56db899aedbd9bd5fc74036467af9d206
Anjalee Kulasinghe <anjaleek@gmail.com>
2024-03-29 08:56:06 +0000

    create herokuapp connection

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b8qmd!52q=$+!hd9!#1q-5+i^+v&(c%aylu))bqd5uq9r=!(%9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-anjaleekula-portfoliopr-jhu3l6gx0y6.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-anjaleekula-portfoliopr-jhu3l6gx0y6.ws-eu110.gitpod.io','.herokuapp.com']
 
 
 # Application definition
